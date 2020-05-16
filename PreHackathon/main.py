bd7b51b1aa5f64fbe6052c46239d4bc6e712e239
Damian A. Gandara <dant.gandara@gmail.com>
2020-05-16 11:41:45 +0200

    Inizacialización de Hackathon Zero

---
+++ PreHackathon/main.py
@@ -0,0 +1,13 @@
+#Declaración de Variables
+nombre = 'Damián A. Gándara' #string
+edad = 28 #integer
+esta_Trabajando = False #boolean
+dato = input('Introduce un dato:') #para solicitar datos en una variable
+print(dato)
+
+edad=edad+20
+#condicionantes if
+if( esta_Trabajando is True ):
+    print(nombre + ' ' + 'Está trabajando: Sí')
+else:
+    print('Pringao')
